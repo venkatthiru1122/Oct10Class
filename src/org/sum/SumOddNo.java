9a4f636424939e9c6251e0380613762b68579fe2
venkatthiru1122 <venkadesht285@gmail.com>
2022-10-11 16:17:10 +0530

    success

---
+++ src/org/sum/SumOddNo.java
@@ -0,0 +1,25 @@
+package org.sum;
+
+public class SumOddNo {
+	
+	public static void main (String[] args) {
+		 int odd=0;
+		for (int i = 1; i <=100; i++) {
+			if (i%2==1) {
+				odd= odd+i;
+
+				
+				
+	
+				
+			}
+			
+			
+			
+		}
+
+		System.out.print("Sum of Odd Numbers : "+odd);
+		
+	}
+
+}
